972d9034d0d5b7a022ecca3aff79e79763dda89d
Robson Martins <robson@robson.com.br>
2025-11-04 00:10:26 +0000

    feat: enable web console access in development environment
    Allow web console access from any IP when running in development mode
    to support Docker and containerized development environments. This is
    in addition to the existing Codespaces configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,7 +62,12 @@ Rails.application.configure do
 
   # Disable host check during development
   config.hosts = nil
-  
+
+  # Allow web console access from any IP in development
+  if Rails.env.development?
+    config.web_console.allowed_ips = %w(0.0.0.0/0 ::/0)
+  end
+
   # GitHub Codespaces configuration
   if ENV['CODESPACES']
     # Allow web console access from any IP
